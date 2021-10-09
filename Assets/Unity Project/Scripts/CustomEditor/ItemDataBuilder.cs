efa76c825c42e1f6db59d1aaec65431a5b7565e1
NoahCedeno <noahcedeno1122@gmail.com>
2021-10-09 01:40:31 -0400

    IMPLEMENTED CSV->ScriptableObjects Workflow for Items
    Ahhh, this automation feels just ***swell***. Now, I have become Prometheus and gifted mankind (also me) the boon of fire (automating writing endless data).
    Thank goodness this works! Now I can do the same thing with CharacterUnits to create friends, enemies, allies, all the same from centralized CSV files that determine all of the pertinent data. This is so cool!!!
    
    :camel:

--- Assets/Unity Project/Scripts/CustomEditor/ItemDataBuilder.cs
+++ Assets/Unity Project/Scripts/CustomEditor/ItemDataBuilder.cs
@@ -2,96 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEditor;
+using System;
 
 public class ItemDataBuilder : MonoBehaviour
 {
-    private const int rumRows = 8;
+    const string ITEMDATA_PATH = @"Assets/Unity Project/ScriptableObjects/Items/";
+    const string HEALINGWEAPON_PATH = @"Assets/Unity Project/ScriptableObjects/Items/Healing/Healing Weapons/";
+    const string HEALINGITEM_PATH = @"Assets/Unity Project/ScriptableObjects/Items/Healing/Healing Items/";
+    const string WEAPON_PATH = @"Assets/Unity Project/ScriptableObjects/Items/Weapons/";
 
+
+    /// <summary>
+    /// Called by a custom inspector button, creates ScriptableObjects from a .csv file of Items.
+    /// </summary>
+    /// <param name="csvString"></param>
     public void BuildItemsFromCSV(string csvString)
     {
         string[] lines = csvString.Split('\n');
-        StringBuilder sb = new StringBuilder();
 
         // For each of the item data lines,
-        for (int i = 1; i < lines.Length - 1; i++)
+        for (int i = 1; i < lines.Length; i++)
         {
             string[] dataValues = lines[i].Split(',');
-            for (int j = 0; j < dataValues.Length - 1; j++)
-            {
-                switch(j)
-                {
-                    // ItemData
-                    case 0: // Name
-                        sb.Append($"Name: {dataValues[j]}, ");
-                        break;
-                    case 1: // ID
-                        sb.Append($"ID: {dataValues[j]}, ");
-                        break;
-                    case 2: // Price
-                        sb.Append($"Price: {dataValues[j]}, ");
-                        break;
-                    case 3: // UsesLeft
-                        sb.Append($"UsesLeft: {dataValues[j]}, ");
-                        break;
-                    case 4: // UsesTotal
-                        sb.Append($"UsesTotal: {dataValues[j]}, ");
-                        break;
-                    case 5: // Icon
-                        sb.Append($"Icon: {dataValues[j]}, ");
-                        break;
-                    // BattleItem
-                    case 6: // BattleItemType
-                        sb.Append($"BattleItemType: {dataValues[j]}, ");
-                        break;
-                    case 7: // Range
-                        sb.Append($"Range: {dataValues[j]}, ");
-                        break;
-                    // ConcreteHealingItem
-                    case 8: // ConcreteHealing Amount
-                        sb.Append($"ConcreteHealing: {dataValues[j]}, ");
-                        break;
-                    case 9: // PercentageHealing Amount
-                        sb.Append($"PercentageHealing: {dataValues[j]}, ");
-                        break;
-                    // Weapon
-                    case 10: // DamageType
-                        sb.Append($"DamageType: {dataValues[j]}, ");
-                        break;
-                    case 11: // WeaponElement
-                        sb.Append($"WeaponElement: {dataValues[j]}, ");
-                        break;
-                    case 12: // BaseDamageAmount
-                        sb.Append($"BaseDamageAmount: {dataValues[j]}, ");
-                        break;
-                    case 13: // Accuracy
-                        sb.Append($"Accuracy: {dataValues[j]}, ");
-                        break;
-                }
-            }
-
-            Debug.Log(sb.ToString());
-            sb.Clear();
+            var itemSO = GetItemTypedInstance(dataValues);
+            CreateItemSOInstance(itemSO, dataValues);
         }
     }
 
     /// <summary>
-    /// Used to get a ScriptableObject instance of a type according to the data filled out.
+    /// Reads from an array of data values for a given item and creates a ScriptableObject for it!
+    /// </summary>
+    /// <param name="so"></param>
+    /// <param name="dataValues"></param>
+    private void CreateItemSOInstance(ScriptableObject so, string[] dataValues)
+    {
+        so.name = dataValues[0];
+        if (so is IItem itemData)
+        {
+            itemData.ItemName = dataValues[0];
+            itemData.ID = int.Parse(dataValues[1]);
+            itemData.Price = int.Parse(dataValues[2]);
+            itemData.UsesLeft = int.Parse(dataValues[3]);
+            itemData.UsesTotal = int.Parse(dataValues[4]);
+
+            if (so is IBattleItem battleItemData)
+            {
+                battleItemData.BattleItemType = (BattleItemType) Enum.Parse(typeof(BattleItemType), dataValues[6]);
+                battleItemData.Range = int.Parse(dataValues[7]);
+                if (so is IConcreteHealing cbhData)
+                {
+                    cbhData.HealingAmount = int.Parse(dataValues[8]);
+                    AssetDatabase.CreateAsset(so, HEALINGWEAPON_PATH + so.name + ".asset");
+                }
+                else if (so is IPercentageHealing pbhData)
+                {
+                    pbhData.PercentageHealing = float.Parse(dataValues[9]);
+                    AssetDatabase.CreateAsset(so, HEALINGWEAPON_PATH + so.name + ".asset");
+                }
+                else if (so is WeaponData weaponData)
+                {
+                    weaponData.DamageType = (DamageType)Enum.Parse(typeof(DamageType), dataValues[10]);
+                    weaponData.WeaponElement = (WeaponElement)Enum.Parse(typeof(WeaponElement), dataValues[11]);
+                    weaponData.BaseDamageAmount = int.Parse(dataValues[12]);
+                    weaponData.Accuracy = float.Parse(dataValues[13]);
+                    AssetDatabase.CreateAsset(weaponData, WEAPON_PATH + weaponData.name + ".asset");
+                }
+            }
+            else
+            {
+                if (so is ConcreteHealingItemData chiData)
+                {
+                    chiData.HealingAmount = int.Parse(dataValues[8]);
+                    AssetDatabase.CreateAsset(chiData, HEALINGITEM_PATH + chiData.name + ".asset");
+                }
+                else if (so is PercentageHealingItemData phiData)
+                {
+                    phiData.PercentageHealing = float.Parse(dataValues[9]);
+                    AssetDatabase.CreateAsset(phiData, HEALINGITEM_PATH + phiData.name + ".asset");
+                }
+                else
+                {
+                    // If the IItem isn't an IBattleItem (inventory item),
+                    AssetDatabase.CreateAsset(so, ITEMDATA_PATH + so.name + ".asset");
+                }
+            }
+        }
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// Used to get a ScriptableObject instance of a type according to the data filled out for an Item.
     /// </summary>
     /// <param name="dataArr">An array of a line of CSV data, split by comma</param>
     /// <returns></returns>
-    private ScriptableObject GetTypedInstance(string[] dataArr)
+    private ScriptableObject GetItemTypedInstance(string[] dataArr)
     {
         bool hasFilledItemData = true;
         for (int i = 0; i <= 5; i++)
         {
-            if (dataArr[i].Equals(""))
+            if (dataArr[i].Equals("") && i != 5)
             {
                 hasFilledItemData = false;
             }
         }
 
         bool hasBattleItemData = true;
-        for (int i = 6; i <= 8; i++)
+        for (int i = 6; i <= 7; i++)
         {
             if (dataArr[i].Equals(""))
             {
@@ -99,11 +117,11 @@ public class ItemDataBuilder : MonoBehaviour
             }
         }
 
-        bool hasConcreteHealingData = !dataArr[9].Equals("");
-        bool hasPercentageHealingData = !dataArr[10].Equals("");
+        bool hasConcreteHealingData = !dataArr[8].Equals("");
+        bool hasPercentageHealingData = !dataArr[9].Equals("");
 
         bool hasWeaponsData = true;
-        for (int i = 11; i <= 14; i++)
+        for (int i = 10; i <= 13; i++)
         {
             if (dataArr[i].Equals(""))
             {
